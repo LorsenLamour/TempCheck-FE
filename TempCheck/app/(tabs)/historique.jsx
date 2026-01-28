d3be13dfae2e7af5c66a77a23e89211f35c25893
Lorsen Valri Deyfker Lamour <2270170@cegepmv.ca>
2026-01-28 15:33:21 -0500

    Added the historic screen

--- TempCheck/app/(tabs)/historique.jsx
+++ TempCheck/app/(tabs)/historique.jsx
@@ -1,10 +1,57 @@
 import React from "react";
-import { View, Text } from "react-native";
+import { View, Text, ScrollView, StyleSheet } from "react-native";
+import list from "../../data/listHistoric.json"
 
 export default function Historique() {
     return (
-        <View style={{ flex: 1, justifyContent: "center", alignItems: "center" }}>
-            <Text>Historique</Text>
-        </View>
+            
+                <ScrollView style={styles.container}>
+                <Text style={styles.textHeader}>Historique des alertes</Text>
+                    {list.map(data =>{
+                        return(
+                            <View key={data.key} style={styles.itemBox}>
+
+                                <Text style={styles.data}>{data.name} {data.temperature} {data.state}</Text> 
+                            
+                             </View>
+                        )
+                    })} 
+                </ScrollView>
+      
     )
 }
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        paddingTop: 50,
+        backgroundColor: "#F8F8E1",
+    },
+    textHeader: {
+        fontSize: 30,
+        fontWeight: "bold",
+        marginBottom: 30,
+        textAlign:"center"
+    },
+    textState: {
+        padding: 20,
+        fontSize: 16,
+        fontWeight: "bold"
+    },
+    data: {
+        fontSize: 15,
+        fontWeight: "bold",
+        margin: 5,  
+              
+    },
+    itemBox: {
+        borderRadius: 10,
+        borderColor: "#000000",
+        borderWidth: 3,
+        margin: 10,
+        backgroundColor: "#F1FBA1",
+    
+       
+    },
+    
+})
