ce762b68bdc78ba3c0cc38c5494ea70a37582b23
Fidel Maindy <fidelmaindygoue1@gmail.com>
2026-05-15 20:14:17 -0400

    URL Public déployée

--- TempCheck/apiConfig.jsx
+++ TempCheck/apiConfig.jsx
@@ -5,7 +5,7 @@ import AsyncStorage from "@react-native-async-storage/async-storage";
 // const BASE_URL = "http://10.10.22.227:5000/api";
 // const BASE_URL = "http://localhost:5000/api";
 // const BASE_URL = "http://localhost:5000/api";
-const BASE_URL = "http://192.168.1.69:5000/api";
+const BASE_URL = "https://tempcheck-bd.up.railway.app/api";
 export const PI_URL = "http://192.168.1.186:5001";
 
 const api = axios.create({
